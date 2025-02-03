feaff7971dc4f72f0d947ba112ceacea976b7879
Noam <noamgoldberg2@gmail.com>
2025-02-03 16:47:07 -0500

    s

--- src/portfolio_optimization/units/report/stock_prices_stats.py
+++ src/portfolio_optimization/units/report/stock_prices_stats.py
@@ -11,13 +11,16 @@ def get_stock_prices_stats(
     stocks_data: Dict[str, Union[Callable, pd.DataFrame]],
     agg: str = "Adj Close",
 ) -> Dict[str, Any]:
-    # st.write("Data keys:", list(stocks_data.keys()))
-    # st.write("Agg:", agg)
+    st.write("Data keys:", list(stocks_data.keys()))
+    st.write("Agg:", agg)
     stocks_data: pd.DataFrame = filter_stocks_df_for_agg(concat_partitions(stocks_data), agg)
-    # st.write("Data Columns:", list(stocks_data.columns))
-    # st.write("Data Shape:", stocks_data.shape)
+    st.write("Data Columns:", list(stocks_data.columns))
+    st.write("Data Shape:", stocks_data.shape)
     start_date = stocks_data.index.min().strftime(DATE_FORMAT)
     end_date = stocks_data.index.max().strftime(DATE_FORMAT)
+    if stocks_data.empty:
+        st.error(f"Failed to retrieve 'stocks_data' dataset (shape: {stocks_data.shape}, columns: {stocks_data.columns})")
+        raise Exception(f"Failed to retrieve 'stocks_data' dataset (shape: {stocks_data.shape}, columns: {stocks_data.columns})")
     stats = stocks_data.describe().loc[['mean', 'std', 'min', 'max']]
     return {
         "start_date": start_date,
